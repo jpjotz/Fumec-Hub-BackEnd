2f48880c00380d3af7af06b44afd41709aecdb1d
João Pedro <srdoog1@gmail.com>
2026-09-05 12:53:47 -0300

    feat: adicionado pool de conexão

--- src/Config/database.js
+++ src/Config/database.js
@@ -6,7 +6,14 @@ const sequelize = new Sequelize(
     process.env.DB_PASSWORD, {
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
-        dialect: 'postgres'
+        dialect: 'postgres',
+
+        pool: {
+            max: 10,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        }
     }
 );
 
