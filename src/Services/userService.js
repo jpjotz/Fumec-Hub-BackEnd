e8cb9a0857ca9ec56ab47c1a8508bde0db837728
João Pedro <srdoog1@gmail.com>
2026-09-04 21:23:07 -0300

    quickfix

--- src/Services/userService.js
+++ src/Services/userService.js
@@ -49,7 +49,7 @@ async function getProfile(userId) {
         throw error;
     }
 
-    return {name: user.name, email: user.email}
+    return {name: user.name, email: user.email, friendCode: user.friendCode}
 }
 
 async function editProfile(userId, data) {
